39d4ceb2a19b88b01f7fdab4bd56ca034f0d84de
Bogdan <programjibogdan@gmail.com>
2026-05-05 10:48:52 +0400

    fixing some minor bugs

--- cred-api/src/auth/auth.module.ts
+++ cred-api/src/auth/auth.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import {
   AuthService,
   AppTokensService,
   GoogleTokensService,
 } from './auth.service';
+import { DatabaseModule } from '../database/database.module';
+import { AppConfigModule } from '../config/config.module';
 
 @Module({
+  imports: [DatabaseModule, AppConfigModule, JwtModule],
   controllers: [AuthController],
   providers: [AuthService, AppTokensService, GoogleTokensService],
   exports: [AppTokensService],
