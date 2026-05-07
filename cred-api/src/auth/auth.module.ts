56b6a80c99a26255c1e115b4dc5ee075a2277b48
Bogdan <programjibogdan@gmail.com>
2026-05-07 18:12:42 +0400

    looks like I got through it

--- cred-api/src/auth/auth.module.ts
+++ cred-api/src/auth/auth.module.ts
@@ -8,9 +8,21 @@ import {
 } from './auth.service';
 import { DatabaseModule } from '../database/database.module';
 import { AppConfigModule } from '../config/config.module';
+import { AppConfigService } from '../config/config.service';
 
 @Module({
-  imports: [DatabaseModule, AppConfigModule, JwtModule],
+  imports: [
+    DatabaseModule,
+    AppConfigModule,
+    JwtModule.registerAsync({
+      imports: [AppConfigModule],
+      inject: [AppConfigService],
+      useFactory: (cfg: AppConfigService) => ({
+        secret: cfg.get('jwt').secret,
+        signOptions: { expiresIn: cfg.get('jwt').expiresIn },
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, AppTokensService, GoogleTokensService],
   exports: [AppTokensService],
