a20976018f86a3bd28156aa4bfce770cdf2ea8be
Bogdan <programjibogdan@gmail.com>
2026-05-29 10:25:55 +0400

    what?

--- cred-web/src/utils/domain.ts
+++ cred-web/src/utils/domain.ts
@@ -1,11 +1,11 @@
-import { config } from '@/config/env'
+import { config } from "@/config/env";
 
 export const isDashboardDomain: boolean = (() => {
-  if (typeof window === 'undefined') return false
-  if (!config.dashboardUrl) return false
+  if (typeof window === "undefined") return false;
+  if (!config.dashboardUrl) return false;
   try {
-    return window.location.hostname === new URL(config.dashboardUrl).hostname
+    return window.location.hostname === new URL(config.dashboardUrl).hostname;
   } catch {
-    return false
+    return false;
   }
-})()
+})();
